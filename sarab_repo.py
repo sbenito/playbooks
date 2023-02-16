505ec30be4984a861634e0ade23a864416e150d6
soar_local_admin <noreply@splunk.com>
2023-02-16 19:01:20 +0000

    asdsad

---
+++ sarab_repo.py
@@ -0,0 +1,50 @@
+"""
+
+"""
+
+
+import phantom.rules as phantom
+import json
+from datetime import datetime, timedelta
+
+
+def on_start(container):
+    phantom.debug('on_start() called')
+
+
+
+    return
+
+def playbook_alert_deescalation_for_test_machines_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("playbook_alert_deescalation_for_test_machines_1() called")
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    # call playbook "sarab SLDKFJ 12223/alert_deescalation_for_test_machines", returns the playbook_run_id
+    playbook_run_id = phantom.playbook("sarab SLDKFJ 12223/alert_deescalation_for_test_machines", container=container)
+
+    return
+
+
+def on_finish(container, summary):
+    phantom.debug("on_finish() called")
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    return
