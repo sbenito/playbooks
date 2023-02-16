72cc571a7b301f0fbb01c6ccb6d6ee53143f3b67
soar_local_admin <noreply@splunk.com>
2023-02-16 18:26:30 +0000

    asd

---
+++ sarab.py
@@ -0,0 +1,50 @@
+"""
+
+"""
+
+
+import phantom.rules as phantom
+import json
+from datetime import datetime, timedelta
+
+
+def on_start(container):
+    phantom.debug('on_start() called')
+
+
+
+    return
+
+def playbook_activedirectory_reset_password_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("playbook_activedirectory_reset_password_1() called")
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    # call playbook "community/activedirectory_reset_password", returns the playbook_run_id
+    playbook_run_id = phantom.playbook("community/activedirectory_reset_password", container=container)
+
+    return
+
+
+def on_finish(container, summary):
+    phantom.debug("on_finish() called")
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    return
